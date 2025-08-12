1c5d3701ce01f8a0720eedea0c5c4ab710e11fdd
Jack Sprinkle <sprinkle.jas@gmail.com>
2025-08-12 16:59:19 -0400

    simple home page edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-export default function Home() {
+export default function HomePage() {
   return (
     <div className="container-sm flex flex-col gap-4">
       <h1 className="text-3xl">Home</h1>
       <p className="pl-4">
         This is a Progressive Web Application. Please add it to your home screen
         to make your life a bit easier. I put some steps below. The goal of this
-        PWA is to have a simple, light weight, phone application that doesn’t
+        PWA is to have a simple, light weight, phone application that doesn&apos;t
         overwhelm you with options, stats, or other information mid-round.
       </p>
       <p className="pl-4">
@@ -32,6 +32,9 @@ export default function Home() {
           <li>
             Offline support is not yet available, but I’m hoping to add it soon.
           </li>
+          <li>
+            Add Handicap Index calculation.
+          </li>
         </ol>
         <p className="text-sm mt-4 italic">
           I&apos;ll do my best to keep this list updated as I encounter issues
@@ -52,7 +55,9 @@ export default function Home() {
         <ol className="list-decimal pl-6">
           <li>Click on the share icon.</li>
           <li>Select &quot;Add to Home Screen&quot; from the menu.</li>
-          <li>Confirm the settings you&apos;d like and then tap &apos;Add&apos;.</li>
+          <li>
+            Confirm the settings you&apos;d like and then tap &apos;Add&apos;.
+          </li>
         </ol>
       </div>
     </div>

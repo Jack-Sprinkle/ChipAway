22e65650207f0af1132c6673c64ebe08aaa48e2e
Jack Sprinkle <sprinkle.jas@gmail.com>
2025-08-13 12:26:58 -0400

    clean up edit club

--- app/_components/EditClub.tsx
+++ app/_components/EditClub.tsx
@@ -7,7 +7,7 @@ import { useLiveQuery } from "dexie-react-hooks";
 export default function EditClub({ editClub, clubId }: EditClubInputProps) {
   const clubToEdit: Club | undefined = useLiveQuery(() =>
     db.clubs.where("id").equals(clubId).first()
-  ) 
+  );
 
   const [club, setClub] = useState<Club>({
     name: "",
@@ -27,64 +27,67 @@ export default function EditClub({ editClub, clubId }: EditClubInputProps) {
     }
   }, [clubId, clubToEdit]);
 
-  const handleChange = (
+  function handleChange(
     evt: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
+  ) {
     evt.preventDefault();
     const { name, value } = evt.target;
-    setClub((prevClub) => ({
-      ...prevClub,
-      [name]:
-        name === "loft" || name === "distance" ? parseFloat(value) : value,
-    }));
-  };
+    setClub({
+      ...club,
+      [name]: value,
+    });
+  }
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
     editClub(club);
   };
-  if (!club) return (<p>Please select club to edit.</p>)
-  return (
-    <div className="container-sm flex justify-center">
-      <form className="flex flex-col items-start" onSubmit={handleSubmit}>
-        <label className="mb-2">
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={club.name || ""}
-            onChange={handleChange}
-            className="ml-2"
-          />
-        </label>
-        <label className="mb-2">
-          Loft:
-          <input
-            type="number"
-            step={0.5}
-            name="loft"
-            value={club.loft || 0}
-            onChange={handleChange}
-            className="ml-2 border rounded p-1 w-20"
-          />
-        </label>
-        <label className="mb-2">
-          Distance:
-          <input
-            type="number"
-            name="distance"
-            value={club.distance || 0}
-            onChange={handleChange}
-            className="ml-2 border rounded p-1 w-20"
-          />
-        </label>
-        <button
-          type="submit"
-          className="rounded-lg bg-yellow-500 text-white px-2 py-1 mt-4"
-        >
-          Save Edit
-        </button>
-      </form>
-    </div>
-  );
+  
+  if (!club) {
+    return <p>Please select club to edit.</p>;
+  } else {
+    return (
+      <div className="container-sm flex justify-center">
+        <form className="flex flex-col items-start" onSubmit={handleSubmit}>
+          <label className="mb-2">
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={club.name || ""}
+              onChange={handleChange}
+              className="ml-2"
+            />
+          </label>
+          <label className="mb-2">
+            Loft:
+            <input
+              type="number"
+              step={0.5}
+              name="loft"
+              value={club.loft || 0}
+              onChange={handleChange}
+              className="ml-2 border rounded p-1 w-20"
+            />
+          </label>
+          <label className="mb-2">
+            Distance:
+            <input
+              type="number"
+              name="distance"
+              value={club.distance || 0}
+              onChange={handleChange}
+              className="ml-2 border rounded p-1 w-20"
+            />
+          </label>
+          <button
+            type="submit"
+            className="rounded-lg bg-yellow-500 text-white px-2 py-1 mt-4"
+          >
+            Save Edit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
